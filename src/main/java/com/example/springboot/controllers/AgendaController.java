b8fab65062761cb0eca57d8a261ee7a39fd63a69
Guilherme Augusto Fernandes <69806054+guigutox@users.noreply.github.com>
2023-08-13 18:49:41 -0300

    Métodos HTTP finalizados

--- src/main/java/com/example/springboot/controllers/AgendaController.java
+++ src/main/java/com/example/springboot/controllers/AgendaController.java
@@ -3,13 +3,14 @@ package com.example.springboot.controllers;
 import com.example.springboot.dtos.AgendaRecordDto;
 import com.example.springboot.models.AgendaModel;
 import com.example.springboot.repositories.AgendaRepository;
-import com.example.springboot.utils.Response;
 import jakarta.validation.Valid;
+import org.apache.logging.log4j.spi.ObjectThreadContextMap;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import com.example.springboot.utils.Response;
 
 import java.util.List;
 import java.util.Optional;
@@ -45,6 +46,32 @@ public class AgendaController {
         }
         response.setData(foundContacts);
         return ResponseEntity.status(HttpStatus.OK).body((Response<List<AgendaModel>>) response);
+    }
+
+    @PutMapping("/agenda/{nome}")
+    public ResponseEntity<Object> updateContato(@PathVariable(value="nome") String nome, @RequestBody @Valid AgendaRecordDto agendaRecordDto){
+            List<AgendaModel> contactO = agendaRepository.findByNome(nome);
+            Response<List<AgendaModel>> response = new Response<>();
+        if(contactO.isEmpty()){
+            response.getErrors().add("Contact not found");
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+        }
+        //Como contact é um array ele vai pegar o elemento de index 0 apenas.
+        var agendaModel = contactO.get(0);
+        BeanUtils.copyProperties(agendaRecordDto, agendaModel);
+        return ResponseEntity.status(HttpStatus.OK).body(agendaRepository.save(agendaModel));
+    }
+
+    @DeleteMapping("/agenda/{nome}")
+    public ResponseEntity<Object> deleteContato(@PathVariable(value = "nome") String nome){
+        List<AgendaModel> contactO = agendaRepository.findByNome(nome);
+        Response<List<AgendaModel>> response = new Response<>();
+        if(contactO.isEmpty()){
+            response.getErrors().add("Contact not found");
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+        }
+        agendaRepository.delete(contactO.get(0));
+        return ResponseEntity.status(HttpStatus.OK).body("Contact deleted sucessfully");
     }
 
 
